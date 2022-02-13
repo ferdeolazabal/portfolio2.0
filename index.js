eeb93f5271ce80a7a4e0788f8855af6c987f3a7c
ferdeolazabal <ferdeolazabal@gmail.com>
2022-02-13 18:12:54 -0300

    script added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 //servir contenido estatico
 app.use( express.static ("public"))
