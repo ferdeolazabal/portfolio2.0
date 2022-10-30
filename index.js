ccfedffc38c5a6672cc46bb0bda7f8ef31a47f89
ferdeolazabal <ferdeolazabal@gmail.com>
2022-10-30 02:48:35 -0300

    Add some proyects

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 //servir contenido estatico
-app.use( express.static ("public"))
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>{
-    res.sendFile( __dirname + '/public/index.html');
-    })
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-app.get('*',  (req, res) => {
-    res.sendFile( __dirname + '/public/404.html');
-    })
-
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-    })
+  console.log(`App listening at http://localhost:${port}`);
+});
